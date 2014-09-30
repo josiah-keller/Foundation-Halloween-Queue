a16968fb03afbb1e175e097f8e2c09273e9bba65
andrew <videogame0201@gmail.com>
2014-09-29 23:56:37 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/', express.static(__dirname+'/public'));
+
+io.on('connection', function(socket){
+    console.log("a user connected");
+    socket.on('disconnect', function(){
+	console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('Listening on port %d', http.address().port);
+});
