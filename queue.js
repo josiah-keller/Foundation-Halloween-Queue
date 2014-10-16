70ba3c93896b90d0264d767939f52ab27025bb74
andrew <videogame0201@gmail.com>
2014-10-16 00:35:27 -0400

    Added emiting to all clients, keeping track of what events changed state, and start of restore admin page

--- queue.js
+++ queue.js
@@ -1,3 +1,9 @@
+var twilio = require('twilio');
+
+var accountSid = 'ACf72e0a36e3a78b1a4301c58a3a62faa3';
+var authToken = "c898fc493f1744a89f3bfb9f9f204536";
+var client = twilio(accountSid, authToken);
+
 var HalloweenQueue = function(){
     this.queue = [];
     this.nextGroup = null;
@@ -15,6 +21,8 @@ HalloweenQueue.prototype.next = function(){
     if(this.queue.length != 0){
 	this.nextGroup = this.queue.shift();
     }
+    this.sendText(this.nextGroup);
+//    this.sendText(this.queue[0]);
 }
 
 HalloweenQueue.prototype.add = function(group){
@@ -39,6 +47,21 @@ HalloweenQueue.prototype.loadState = function(state){
     this.nextGroup = state.nextGroup;
     this.queue = state.queue;
     this.done = state.done;
+}
+
+HalloweenQueue.prototype.sendText = function(group){
+    console.log(group)
+    client.messages.create({
+        body: "Your group is next in the Haunted Maze!",
+        to: "+16306975879",
+        from: "+17085723531"
+    }, function(err, message) {
+        if(err){
+            console.log(err);
+        }else{
+            console.log(message.sid);
+        }
+    });
 }
 
 exports.HalloweenQueue = HalloweenQueue;
