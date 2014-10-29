ed4ae466929741ab0f2d7823ad51c45548de9f47
andrew <videogame0201@gmail.com>
2014-10-29 18:41:22 -0400

    Added second column of names to display

--- public/app/display.js
+++ public/app/display.js
@@ -6,28 +6,33 @@ define(function(require) {
     function display(){
     	var self = this;
 
-    	self.nextGroup = ko.observableArray([]);
     	self.queuedGroups = ko.observableArray([]);
-
-    	self.getNextGroupName = function(){
-    		if(self.nextGroup()) return "1. " + self.nextGroup().name + " - " + self.nextGroup().groupSize;
-    	}
+        self.queuedGroups2 = ko.observableArray([]);
 
     	self.getQueuedGroupName = function(index){
-    		if(self.nextGroup()){
-    			var num = index+2
-    		}else{
-    			var num = index+1
-    		}
+    	    var num = index+1
     		var group = self.queuedGroups()[index];
-    		console.log(group)
     		return num + ". " + group.name + " - " + group.groupSize;
     	}
 
+        self.getQueuedGroup2Name = function(index){
+            var num = index+18
+            var group = self.queuedGroups2()[index];
+            return num + ". " + group.name + " - " + group.groupSize;
+        }
+
     	app.data.on("state", function(data){
-            console.log('state change', data);
-            self.nextGroup(data.nextGroup);
-            self.queuedGroups(data.queue);
+            self.queuedGroups([]);
+            self.queuedGroups2([]);
+            self.queuedGroups.unshift(data.nextGroup);
+            for(var i = 0; i<data.queue.length; i++){
+                if(i<16){
+                    self.queuedGroups.push(data.queue[i]);
+                }else{
+                    self.queuedGroups2.push(data.queue[i]);
+                }
+            }
+            
         });
 
         self.activate = function(){
