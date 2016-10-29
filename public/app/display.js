b520f61f1826d4f997d757b9e222af0f06a9bd53
Josiah Keller <josiah@josiahkeller.com>
2016-10-28 23:38:26 -0400

    Add maze display

--- public/app/display.js
+++ public/app/display.js
@@ -3,36 +3,17 @@ define(function(require) {
         ko = require('knockout'),
         system = require('durandal/system');
 
-    function display(){
-    	var self = this;
+    function display() {
+        var self = this;
 
-    	self.queuedGroups = ko.observableArray([]);
-        self.queuedGroups2 = ko.observableArray([]);
+        self.mazeQueue = ko.observableArray([]);
 
-    	self.getQueuedGroupName = function(index){
-    	    var num = index+1
-    		var group = self.queuedGroups()[index];
-    		return num + ". " + group.name + " - " + group.groupSize;
-    	}
-
-        self.getQueuedGroup2Name = function(index){
-            var num = index+18
-            var group = self.queuedGroups2()[index];
-            return num + ". " + group.name + " - " + group.groupSize;
-        }
-
-    	app.data.on("state", function(data){
-            self.queuedGroups([]);
-            self.queuedGroups2([]);
-            if(data.nextGroup) self.queuedGroups.unshift(data.nextGroup);
-            for(var i = 0; i<data.queue.length; i++){
-                if(i<16){
-                    self.queuedGroups.push(data.queue[i]);
-                }else{
-                    self.queuedGroups2.push(data.queue[i]);
-                }
-            }
-            
+    	app.data.on("state", function(data) {
+            self.mazeQueue([]);
+            if (data.maze.nextGroup) self.mazeQueue.unshift(data.maze.nextGroup);
+            data.maze.queue.forEach(function(group) {
+                self.mazeQueue.push(group);
+            });
         });
 
         self.activate = function(){
@@ -41,5 +22,4 @@ define(function(require) {
     }
 
     return display;
-
 });
